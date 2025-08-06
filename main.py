d177fed68ea7b52ece91ffb49740e69e14200d61
elitetouch <elitetouchconcept@gmail.com>
2025-08-06 16:04:44 +0100

    updated python code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+#print("hello world, abeokuta i am learning python!")
+
+#name = nput("Enter your name ? : ")
+
+#rint(f"welcome to python {name} !")
+
+name = "peter"
+
+print(f"my name is {name} !")
