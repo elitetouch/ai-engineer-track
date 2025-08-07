2e7a3221bbc329e252248fbf57870b7b3074eab3
elitetouch <elitetouchconcept@gmail.com>
2025-08-07 12:53:15 +0100

    variable updated

---
+++ restaurant_task.py
@@ -0,0 +1,9 @@
+# welcome the customer with the restaurant name
+# take the customer's name and order
+# calculate the cost of the order  
+
+print("Welcome to the Madam Crown's Kitchen!")
+customer_name = input("Please enter your name: ")
+print("Hello,", customer_name)
+order = input("What would you like to order? ")
+print(f"{order} will cost N1500. Thank you for your order, {customer_name}!")
---
+++ using_input_statement.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+
+
+print("Welcome to python programming", name)
+
+#converting int, float
+age = int(input("Enter your age: "))
+print("Your age is", age)
+print(f"You will be {age + 2} years old in 2 years.")
+
+exam_score = float(input("Enter your exam score: "))
+print("Your exam score is", exam_score)
+
+
+#calculator using input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}"  )
