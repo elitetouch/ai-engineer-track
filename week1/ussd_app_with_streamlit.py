7cc0c0c826d5b24685e7b7b22a9c26130bc55f5e
Elitetouch <elitetouchconcept@gmail.com>
2025-08-08 19:13:55 +0100

    deleted image in kuda bank

--- week1/ussd_app_with_streamlit.py
+++ week1/ussd_app_with_streamlit.py
@@ -1,16 +1,7 @@
 import streamlit as st
 
-from PIL import Image
 
 st.title("KUDA BANK Application")
-
-# Upload image file
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-if uploaded_file is not None:
-    # Open and display the image
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
 
 
 balance = 1000
