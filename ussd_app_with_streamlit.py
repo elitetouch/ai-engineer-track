5c467bf77b0daaf67191acf8681ba4764d9a72f6
elitetouch <elitetouchconcept@gmail.com>
2025-08-08 13:48:30 +0100

    added image

--- ussd_app_with_streamlit.py
+++ ussd_app_with_streamlit.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
+from PIL import Image
+
 st.title("USSD Application")
+
+# Upload image file
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Open and display the image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
 
 balance = 1000
 
