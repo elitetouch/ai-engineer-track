55b3695c5e747e590db22186c0106b2084c876d5
Elitetouch <elitetouchconcept@gmail.com>
2025-08-17 20:59:47 +0100

    task4 (working_with_set)

--- my_tasks/week2/task4/task4.py
+++ my_tasks/week2/task4/task4.py
@@ -1,23 +1,30 @@
-
+#Task4: Create a Unique Voters Registration System
 # created by peter okonmah
 
-# Ask for voter names and store in a set.
-
+# initialize voters as set
 voters = set()
 
-voter = input("Enter voter's name: ")
-# voters.add(voter)
+# Ask user how many voters you want to register
+number_of_voter = int(input("How many voters do you want to register? "))
 
-if voter not in voters:
-   voters.add(voter)
-   print(f"'{voter}' registered successfully.")
-   voter = input("Enter voter's name: ")
+# Loop to register voters in range of number of voters
+for i in range(number_of_voter):
   
-
-else:
-   print(f"'{voter}' already registered.")
-   voter = input("Enter another voter's name: ")
-   voters.add(voter)
+    # Ask for voter name.
+    name = input(f"Enter name of voter {i+1}: ")
+    
+    # Check if the voter is already registered, display a warning
+    if name in voters:
+        print("This voter is already registered!")
+        
+        # Ask for another voter name.
+        name = input(f"Enter another name of voter {i+1}: ")
+        voters.add(name)
+    else:
+      
+      # store in a set if not already registered
+        voters.add(name)
+        print(f"{name} has been registered successfully.")
 
 # After registration, display the total number of unique voters.
-print(f"Total unique voters registered: {len(voters)}")
+print(f"Registration complete.\n Total unique voters: {len(voters)}")
